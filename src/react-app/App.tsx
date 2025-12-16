fbeb8c64896e95166616d1d710c1f1b6036d05d3
stoneyangzh <stoneyangzh@163.com>
2025-12-16 07:23:07 -0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -2,9 +2,7 @@
 
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
+
 import "./App.css";
 
 function App() {
