ef29d6d53a7f707c3a78bc85dfcd342d6ae96672
stoneyangzh <stoneyangzh@163.com>
2025-12-16 07:20:42 -0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -13,25 +13,7 @@ function App() {
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-				<a href="https://hono.dev/" target="_blank">
-					<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-				</a>
-				<a href="https://workers.cloudflare.com/" target="_blank">
-					<img
-						src={cloudflareLogo}
-						className="logo cloudflare"
-						alt="Cloudflare logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React + Hono + Cloudflare</h1>
+			<h1>Welcome to AI Singers</h1>
 			<div className="card">
 				<button
 					onClick={() => setCount((count) => count + 1)}
@@ -58,7 +40,7 @@ function App() {
 					Edit <code>worker/index.ts</code> to change the name
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p>
+			<p className="read-the-docs">AI singers</p>
 		</>
 	);
 }
